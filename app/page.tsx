0843ea5f66dc3fc6d6f24af5ba3451c768b2bfd2
christian <cpatino@leanfactory.co>
2025-01-31 20:28:58 -0500

    Add: taskForm to create and edit a task, unify components, display table and form with buttons v1

--- app/page.tsx
+++ app/page.tsx
@@ -1,98 +1,20 @@
 "use client";
-import Button from "./components/button";
-import { useState } from "react";
-import TaskList from "./components/list";
+
+import TaskList from "../components/list";
+import TaskForm from "@/components/taskForm";
 
 export default function Home() {
-  const [loading, setLoading] = useState(false);
-  const [message, setMessage] = useState("");
-
-  const handlerCreateTask = async () => {
-    setLoading(true);
-    setMessage("");
-
-    const response = await fetch("/api/tasks", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        title: "Nueva tarea 3",
-        description: "Detalles de la tarea 3",
-      }),
-    });
-
-    if (response.ok) {
-      setMessage("✅ Tarea creada correctamente");
-    } else {
-      setMessage("❌ Error al crear la tarea");
-    }
-
-    setLoading(false);
-  };
-  const handlerEditTask = async () => {
-    setLoading(true);
-    setMessage("");
-
-    const response = await fetch("/api/tasks", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        title: "Nueva tarea",
-        description: "Detalles de la tarea",
-      }),
-    });
-
-    if (response.ok) {
-      setMessage("✅ Tarea creada correctamente");
-    } else {
-      setMessage("❌ Error al crear la tarea");
-    }
-
-    setLoading(false);
-  };
-  const handleDeleteTask = async () => {
-    setLoading(true);
-    setMessage("");
-
-    const response = await fetch("/api/tasks", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        title: "Nueva tarea",
-        description: "Detalles de la tarea",
-      }),
-    });
-
-    if (response.ok) {
-      setMessage("✅ Tarea creada correctamente");
-    } else {
-      setMessage("❌ Error al crear la tarea");
-    }
-
-    setLoading(false);
-  };
   return (
-    
     <div className="flex flex-col justify-center items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col items-center justify-center h-full w-full">
         <h1 className="text-3xl font-bold mb-4">
-          Bienvenido a la App de Tareas
+          Bienvenido a la Web de Tareas
         </h1>
-
-        <div className="p-4">
-          <h1 className="text-2xl font-bold mb-4">Lista de Tareas</h1>
-          <Button onClick={handlerCreateTask} disabled={loading}>
-            {loading ? "Creando..." : "Crear Tarea"}
-          </Button>
-          <Button onClick={handlerEditTask} disabled={loading}>
-            {loading ? "Actualizando..." : "Actualizar Tarea"}
-          </Button>
-          <Button onClick={handleDeleteTask} disabled={loading}>
-            {loading ? "Borrando..." : "Borrar Tarea"}
-          </Button>
-          {message && <p className="mt-2">{message}</p>}
+        <div className="p-8 max-w-4xl mx-auto">
+          <TaskForm />
         </div>
         <div className="p-8 max-w-4xl mx-auto">
-          <h1 className="text-3xl font-bold mb-6">Lista</h1>
+          <h1 className="text-3xl font-bold mb-6">Lista de tareas: </h1>
           <TaskList />
         </div>
       </main>
